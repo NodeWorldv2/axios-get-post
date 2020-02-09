044d6041a3e41a05fdd2ce8a33057b0f31796bf9
linuxgeekup <linuxgeekup@gmail.com>
2020-02-09 17:21:33 +0600

    completing the axios request

--- src/App.js
+++ src/App.js
@@ -1,16 +1,61 @@
-import React from 'react';
+import React,{Component} from 'react';
 import './App.css';
+import axios from 'axios'
+
 
 import NewComponent from './components/new_components';
 import StateCom from './components/state_component';
+import Axiostest from './components/state_component';
 
-function App() {
-  return (
-    <div className="App">
-       <NewComponent/>
-       <StateCom/>
-    </div>
-  );
+
+class App extends Component{
+
+  state = {
+    posts: []
 }
+
+componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/todos/')
+        .then(response => {
+            this.setState({
+                posts:response.data
+            })
+        })
+        .catch(error=>console.log(error))
+}
+
+
+  render(){
+
+    let {posts}=this.state;
+
+
+      if(posts.length===0){
+          return (
+            <div className="App">
+                <h1>Loading....</h1>
+            </div>
+          );
+      }
+      else {
+        console.log(posts);
+        return (
+          <div className="App">
+            {posts.map(post=> <li className='list-group-item'>{post.title} </li>)}
+          </div>
+        );
+      }
+
+    }
+}
+
+// function App() {
+//   return (
+//     <div className="App">
+//        <NewComponent/>
+//        <Axiostest/>
+//     </div>
+//   );
+// }
 
 export default App;
---
+++ src/components/axios.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import axios from 'axios'
+
+class axios extends Component {
+
+    
+
+
+    render() {
+        return (
+            <div>
+                <h4>this is from axios</h4>
+            </div>
+        );
+    }
+}
+
+export default axios;
